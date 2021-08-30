66f3b4f369112666a44164f5661fb65b561e3968
200ohm <skrbnch102928>
2021-08-31 08:10:24 +0900

    変数名変更

--- main.cpp
+++ main.cpp
@@ -7,8 +7,8 @@
 void msleep(int ms) {
 	struct timespec ts;
 
-	ts.tv_sec = ns / 1000;
-	ts.tv_nsec = ns % 1000;
+	ts.tv_sec = ms / 1000;
+	ts.tv_nsec = (ms % 1000) * 1000000;
 
 	nanosleep(&ts, NULL);
 }
