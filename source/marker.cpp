20e3a27bded464b7963165a03c48ea37e86b0876
200ohm <skrbnch102928>
2021-09-07 15:06:23 +0900

    パラメータ調整15:06

--- source/marker.cpp
+++ source/marker.cpp
@@ -123,7 +123,7 @@ int main(void) {
           cout << "左の辺の長さ:" << llen << endl;
           cout << "右の辺の長さ:" << rlen << endl;
           cout << "平均:" << mlen << endl;
-          cout << "距離:" << 2860 / mlen + 5 << "cm" << endl;
+          cout << "距離:" << 2900 / mlen + 5 << "cm" << endl;
           cout << endl;
 
           cout << "1=黒/0=白の行列" << endl;
