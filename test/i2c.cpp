c7ebe0306da5b49fb0f10ce5ec538b83e43d837f
200ohm <skrbnch102928>
2021-11-09 11:32:05 +0900

    100ステップ回転

--- test/i2c.cpp
+++ test/i2c.cpp
@@ -5,11 +5,11 @@ int main(void) {
   int ID = 0x11;
   int fd = wiringPiI2CSetup(ID);
 
-  while (1) {
+  // while (1) {
     if ((wiringPiI2CWriteReg8(fd, 0x00, 0x01)) < 0){
       std::cout << "write error" << std::endl;
     } else {
       std::cout << "write \"0x52\"" << std::endl;
     }
-  }
+  // }
 }
