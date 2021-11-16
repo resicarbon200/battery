806d5339ea3029f8b7437086d9a828f76a36567f
200ohm <skrbnch102928>
2021-11-16 14:42:39 +0900

    マーカーが移動体の方を向いている場合の動作

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@
 
 const float TAR_DEPTH = 20.0;   //目標距離
 const float TOL_DEPTH = 2.0;    //距離許容差
+const float TOL_ANGLE = 10.0;   //角度許容差
 const float TOL_DEFLEC = 0.3;   //カメラ角度許容差
 
 void msleep(int ms) {
@@ -49,37 +50,70 @@ int main(void) {
       // std::cout << pm->getAngle() << std::endl;   //角度を表示
       std::cout << pm->getDeflec();  //中心座標が右寄りなら正の数，左よりなら負の数を表示
 
-      if (pm->getDepth() > TAR_DEPTH + TOL_DEPTH) {     //マーカーが遠いとき
-        if ((wiringPiI2CWriteReg8(fd, 0x00, 0x01)) < 0){
+      //============================================================
+      //移動制御
+
+      if (-TOL_ANGLE < pm->getAngle() && pm->getAngle() < TOL_ANGLE) {    //マーカーが移動体の方を向いているとき
+        if (ret = 0) {    //カメラが移動体の正面方向を向いているとき
+
+          if (pm->getDepth() > TAR_DEPTH + TOL_DEPTH) {     //マーカーが遠いとき
+            if ((wiringPiI2CWriteReg8(fd, 0x00, 0x01)) < 0){
+              std::cout << "write error" << std::endl;
+            } else {
+              // std::cout << "write \"0x01\"" << std::endl;
+            }
+          }
+
+          if (pm->getDepth() < TAR_DEPTH - TOL_DEPTH) {     //マーカーが近いとき
+            if ((wiringPiI2CWriteReg8(fd, 0x00, 0x02)) < 0){
+              std::cout << "write error" << std::endl;
+            } else {
+              // std::cout << "write \"0x0a\"" << std::endl;
+            }
+          }
+
+        } else {    //カメラが移動体の正面方向を向いていないとき
+
+          if (ret < 0) {     //カメラが右を向いているとき
+            if ((wiringPiI2CWriteReg8(fd, 0x00, 0x04)) < 0){
+              std::cout << "write error" << std::endl;
+            } else {
+              // std::cout << "write \"0x04\"" << std::endl;
+            }
+          }
+          
+          if (ret > 0) {     //カメラが左を向いているとき
+            if ((wiringPiI2CWriteReg8(fd, 0x00, 0x03)) < 0){
+              std::cout << "write error" << std::endl;
+            } else {
+              // std::cout << "write \"0x03\"" << std::endl;
+            }
+          }
+
+      } else { //マーカーが移動体の方を向いていないとき
+
+      }
+
+      //============================================================
+      //カメラ回転
+
+      if (pm->getDeflec() > CAMERA_DEF) {     //マーカーが視界右方のとき
+        if ((wiringPiI2CWriteReg8(fd, 0x00, 0x0a)) < 0){
           std::cout << "write error" << std::endl;
         } else {
           // std::cout << "write \"0x0a\"" << std::endl;
         }
       }
 
-      if (pm->getDepth() < TAR_DEPTH - TOL_DEPTH) {     //マーカーが近いとき
-        if ((wiringPiI2CWriteReg8(fd, 0x00, 0x02)) < 0){
+      if (pm->getDeflec() < -CAMERA_DEF) {     //マーカーが視界左方のとき
+        if ((wiringPiI2CWriteReg8(fd, 0x00, 0x09)) < 0){
           std::cout << "write error" << std::endl;
         } else {
-          // std::cout << "write \"0x0a\"" << std::endl;
+          // std::cout << "write \"0x09\"" << std::endl;
         }
       }
 
-      // if (pm->getDeflec() > CAMERA_DEF) {     //マーカーが視界右方のとき
-      //   if ((wiringPiI2CWriteReg8(fd, 0x00, 0x0a)) < 0){
-      //     std::cout << "write error" << std::endl;
-      //   } else {
-      //     // std::cout << "write \"0x0a\"" << std::endl;
-      //   }
-      // }
-
-      // if (pm->getDeflec() < -CAMERA_DEF) {     //マーカーが視界左方のとき
-      //   if ((wiringPiI2CWriteReg8(fd, 0x00, 0x09)) < 0){
-      //     std::cout << "write error" << std::endl;
-      //   } else {
-      //     // std::cout << "write \"0x09\"" << std::endl;
-      //   }
-      // }
+      //============================================================
     } else {
       std::cout << "?";
     }
